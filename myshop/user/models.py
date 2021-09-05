c1523cdf0ace5e873db879208ce61834d58190ac
Ale <alerodriguezgarcia@me.com>
2021-09-05 14:33:25 -0600

    Personal and main information about the user.

--- myshop/user/models.py
+++ myshop/user/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    """
+    Personal and main information about the user.
+
+        first_name = Their first name.
+        last_name = Their last name.
+        genre = Their genre name.
+        birthday = Their birth day.
+    """
+    GENRES = (
+        ('M', 'Masculine'),
+        ('F', 'Femenine'),
+    )
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    genre = models.CharField(max_length=1, choices=GENRES)
+    birthday = models.DateField()
