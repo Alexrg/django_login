57da1e800ae49df744962001449904691d60a303
Ale <alerodriguezgarcia@me.com>
2021-09-05 16:25:55 -0600

    User information.

--- myshop/user/models.py
+++ myshop/user/models.py
@@ -18,3 +18,15 @@ class Person(models.Model):
     last_name = models.CharField(max_length=50)
     genre = models.CharField(max_length=1, choices=GENRES)
     birthday = models.DateField()
+
+class User(models.Model):
+    """
+    User information.
+
+        id_person = Relation between the user and it's data8u
+        email = User's email.
+        password = User's password.
+    """
+    id_person = models.ForeignKey("Person", related_name='users', on_delete=models.CASCADE)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=50)
