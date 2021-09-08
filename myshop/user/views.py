abe9f54aa81cc0f17ff3c6cc92b1309501174743
Ale <alerodriguezgarcia@me.com>
2021-09-07 19:57:06 -0600

    Prompt the form into the login view.

--- myshop/user/views.py
+++ myshop/user/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .forms import UserLogin
 
+def login(request):
+    form = UserLogin()
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the user login page")
+    if request.method == 'POST':
+        pass
+    else: 
+        form = UserLogin()
+        
+    return render(request, "user_login.html", {'form': form})
